6d268b2bc8fe78633d86ed4038bad9829ab06bfa
reversemountain-dev <skudy@korea.ac.kr>
2020-11-17 21:39:01 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-
-
 import 'package:flutter/gestures.dart';
+import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_validator/email_validator.dart';
 import 'page1.dart';
 import 'page2.dart';
 import 'page3.dart';
@@ -55,7 +57,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   // ignore: missing_return
   Future<void> incrementCounter() {
     //FirebaseFirestore.instance.collection('email').add({'email': 'email$_counter'});
@@ -90,6 +91,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Listener(
         onPointerSignal: (PointerSignalEvent event) {
@@ -100,29 +103,273 @@ class _MyHomePageState extends State<MyHomePage> {
               _moveUp();
             }
           }
-
         },
-        child: PageView(
-
-          pageSnapping: true,
-          scrollDirection: Axis.vertical,
-          controller: _pageController,
+        child: Stack(
           children: [
-            Page1(),
-            Page2(),
-            Page3(),
-            Page4(),
+            PageView(
+              pageSnapping: true,
+              scrollDirection: Axis.vertical,
+              controller: _pageController,
+              children: [
+                Page1(),
+                Page2(),
+                Page3(),
+                Page4(),
+              ],
+            ),
+            height < width
+                ? Container(child: null)
+                : Container(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      color: Color(0xFF533AFF),
+                      width: MediaQuery.of(context).size.width,
+                      height: 67,
+                      child: FlatButton(
+                        child: Container(
+                            width: MediaQuery.of(context).size.width / 2,
+                            child: Image.network(
+                                "/assets/assets/desc/4page_button_mobile.svg",
+                                width: MediaQuery.of(context).size.width / 2)),
+                        onPressed: (() {
+                          _showDialog();
+                        }),
+                      ),
+                    ),
+                  ),
           ],
         ),
       ),
     );
   }
 
-  void _handleSubmitted(String text) {
-    /*FirebaseFirestore.instance.collection('email').add({
-          'email': text,
-          'datetime': DateTime.now()
-      });*/
-    _textController.clear();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _hobbyController = TextEditingController();
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        var _isButtonEnabled = false;
+
+        return StatefulBuilder(builder: (context, setState) {
+          void _validate(String value) {
+            if (_hobbyController.text != "" &&
+                _nameController.text != "" &&
+                EmailValidator.validate(_emailController.text)) {
+              setState(() {
+                _isButtonEnabled = true;
+              });
+            } else {
+              setState(() {
+                _isButtonEnabled = false;
+              });
+            }
+          }
+
+          return BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0))),
+              content: Container(
+                width: 350.0,
+                height: 500.0,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 15),
+                      Text(
+                        "Palette 신청하기",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(height: 35),
+                      Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, spreadRadius: 1),
+                          ],
+                        ),
+                        height: 30,
+                        width: 220,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                alignment: Alignment.center,
+                                height: 20,
+                                width: 170,
+                                child: TextField(
+                                  autofocus: false,
+                                  onChanged: _validate,
+                                  controller: _nameController,
+                                  decoration: new InputDecoration.collapsed(
+                                      hintText: "이름"),
+                                ),
+                              ),
+                              Container(
+                                width: 30,
+                                child: Icon(Icons.check,
+                                    color: _nameController.text != ""
+                                        ? Colors.green
+                                        : Colors.transparent),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, spreadRadius: 1),
+                          ],
+                        ),
+                        height: 30,
+                        width: 220,
+                        child: Center(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  height: 20,
+                                  width: 170,
+                                  child: TextField(
+                                    autofocus: false,
+                                    onChanged: _validate,
+                                    controller: _emailController,
+                                    decoration: new InputDecoration.collapsed(
+                                        hintText: "이메일"),
+                                  ),
+                                ),
+                                Container(
+                                  width: 30,
+                                  child: Icon(Icons.check,
+                                      color: EmailValidator.validate(
+                                              _emailController.text)
+                                          ? Colors.green
+                                          : Colors.transparent),
+                                ),
+                              ]),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, spreadRadius: 1),
+                          ],
+                        ),
+                        height: 30,
+                        width: 220,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                alignment: Alignment.center,
+                                height: 20,
+                                width: 170,
+                                child: TextField(
+                                  autofocus: false,
+                                  onChanged: _validate,
+                                  controller: _hobbyController,
+                                  decoration: new InputDecoration.collapsed(
+                                      hintText: "취미"),
+                                ),
+                              ),
+                              Container(
+                                width: 30,
+                                child: Icon(Icons.check,
+                                    color: _hobbyController.text != ""
+                                        ? Colors.green
+                                        : Colors.transparent),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                          height: 50,
+                          child: Text("앱 신청은 이메일 수신에 \n동의 함을 뜻합니다.")),
+                      SizedBox(height: 15),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 286,
+                        height: 80,
+                        child: FlatButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          color: Color(0xFF533AFF),
+                          height: 60,
+                          minWidth: 150,
+                          disabledColor: Colors.grey,
+                          onPressed: _isButtonEnabled ? _submit : null,
+                          child: Text(
+                            "신청",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
+      },
+    );
+  }
+
+  void _submit() {
+    FirebaseFirestore.instance.collection('email').add({
+      'name': _nameController.text,
+      'email': _emailController.text,
+      'hobby': _hobbyController.text,
+      'datetime': DateTime.now()
+    });
+    Navigator.of(context, rootNavigator: true).pop();
+    _thankyou();
+  }
+
+  void _thankyou() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30.0))),
+            content: Container(
+              width: 350.0,
+              height: 200.0,
+              child: Center(
+                  child: Image.network("/assets/assets/desc/thankyou.svg",
+                      width: 300)),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
--- lib/page3.dart
+++ lib/page3.dart
@@ -13,6 +13,8 @@ class _Page3State extends State<Page3> {
   var _opacity2 = 0.0;
   var _right1 = 200.0;
   var _right2 = 600.0;
+  var _right3 = 400.0;
+
   var _top = 50.0;
   @override
   void initState() {
@@ -34,6 +36,7 @@ class _Page3State extends State<Page3> {
     setState(() {
       _opacity2 = 1;
       _right2 = 300;
+      _right3 = 200;
     });
   }
 
@@ -209,44 +212,88 @@ class _Page3State extends State<Page3> {
                 ),
               ),
             ))
-        : Container(
-            alignment: Alignment.center,
-            color: Color(0xFFFFFFFF),
-            child: Center(
-              child: Container(
-                  child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 10),
-                    child: Image.network(
-                        "/assets/assets/desc/3page_text_mobile.svg"),
-                  ),
-                  AnimatedOpacity(
-                    opacity: _opacity1,
-                    duration: Duration(milliseconds: 500),
-                    child: AnimatedContainer(
-                      margin: EdgeInsets.only(top: _right1),
+        : Stack(children: [
+          Container(
+              alignment: Alignment.center,
+              child: Center(
+                child: Container(
+                    child: Column(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 10),
+                        child: null),
+                    AnimatedOpacity(
+                      opacity: _opacity2,
                       duration: Duration(milliseconds: 500),
-                      child: Image.network(
-                        "/assets/assets/images/3page1.png",
-                        loadingBuilder: (BuildContext context, Widget child,
-                            ImageChunkEvent loadingProgress) {
-                          if (loadingProgress == null) return child;
-                          return Center(
-                            child: CircularProgressIndicator(
-                              value: loadingProgress.expectedTotalBytes != null
-                                  ? loadingProgress.cumulativeBytesLoaded /
-                                      loadingProgress.expectedTotalBytes
-                                  : null,
-                            ),
-                          );
-                        },
+                      child: AnimatedContainer(
+                        margin: EdgeInsets.only(
+                            top: _right3,
+                            left: MediaQuery.of(context).size.width * 0.3),
+                        duration: Duration(milliseconds: 500),
+                        child: Image.network(
+                          "/assets/assets/images/3page2.png",
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return Center(
+                              child: CircularProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes !=
+                                        null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        loadingProgress.expectedTotalBytes
+                                    : null,
+                              ),
+                            );
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )),
+                  ],
+                )),
+              ),
             ),
-          );
+            Container(
+              alignment: Alignment.center,
+              child: Center(
+                child: Container(
+                    child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 10),
+                      child: Image.network(
+                          "/assets/assets/desc/3page_text_mobile.svg"),
+                    ),
+                    AnimatedOpacity(
+                      opacity: _opacity1,
+                      duration: Duration(milliseconds: 500),
+                      child: AnimatedContainer(
+                        margin: EdgeInsets.only(
+                            top: _right1,
+                            right: MediaQuery.of(context).size.width * 0.3),
+                        duration: Duration(milliseconds: 500),
+                        child: Image.network(
+                          "/assets/assets/images/3page1.png",
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return Center(
+                              child: CircularProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes !=
+                                        null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        loadingProgress.expectedTotalBytes
+                                    : null,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+            ),
+            
+          ]);
   }
 }
